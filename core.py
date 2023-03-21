161df8cdfb112c5a555ee94a493eedaffe6df773
Ben Thompson <ben.t11@hotmail.com>
2023-03-20 20:53:50 -0700

    add rudimentary detection of my cat, motor skeletons

--- core.py
+++ core.py
@@ -16,7 +16,14 @@ GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.setup(MOTOR_PIN, GPIO.OUT)
 GPIO.setup(SWITCH_PIN, GPIO.IN)
 
-upper_color_bound = numpy.array([30, 154, 154])
+lower_color_bound = numpy.array([14, 0, 0])
+upper_color_bound = numpy.array([30, 154, 154]) # my cat's coat color
+
+def openTray():
+    # 
+
+def closeTray():
+    #
 
 while True:
     if GPIO.input(SWITCH_PIN):
@@ -30,9 +37,20 @@ while True:
                 feed = PiRGBArray(cam, size = (1920, 1080))
                 time.sleep(1)
                 
-                for frame in cam.capture_continuous(feed, format="bgr", use_video_port=True):
+                for frame in cam.capture_continuous(feed, format = "bgr", use_video_port = True):
                     image = frame.array
-                    cv2.imshow("Frame", image)
+                    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+                    mask = cv2.inRange(hsv_image, lower_color_bound, upper_color_bound)
+                    if numpy.sum(mask) > 200000: # my cat detected! 500x400 square of her coat
+                        # potentially disable light, might mess with their vision more
+                        openTray()
+                        time.sleep(2) # wait for tray to open
+                        while GPIO.input(SWITCH_PIN):
+                            time.sleep(1)
+
+                        closeTray()
+                        break
+
                     # clear stream
                     feed.truncate(0)
         except:
