cc7ee6c6f7938a882858a1f8a0868a0c7b3ad7e5
Ben Thompson <ben.t11@hotmail.com>
2023-03-21 10:52:49 -0700

    fix syntax errors, make sure motors are initialized properly

--- core.py
+++ core.py
@@ -19,7 +19,7 @@ GPIO.setup(SWITCH_PIN, GPIO.IN)
 MOTOR_PINS = [27, 29, 31, 33]
 TRAY_EXTEND_DEGREES = 360 * 6 # 4096 for full rotation
 STEPS_PER_ROTATION = 4096
-STEP_TIME = 0.002 # time to wait between each step
+STEP_TIME = 0.001 # 0.002 # time to wait between each step
 # if set too low, steps will be skipped
 STEP_SEQUENCE = [[1, 0, 0, 1],
                  [1, 0, 0, 0],
@@ -30,7 +30,7 @@ STEP_SEQUENCE = [[1, 0, 0, 1],
                  [0, 0, 1, 1],
                  [0, 0, 0, 1]]
 
-for i in range(3):
+for i in range(len(STEP_SEQUENCE)):
     print(i)
     GPIO.setup(MOTOR_PINS[i], GPIO.OUT)
     GPIO.output(MOTOR_PINS[i], GPIO.LOW)
@@ -44,6 +44,7 @@ CAMERA_Y_RES = 1088
 motor_steps = 0
 
 def runMotor(degrees, direction): # direction: true is cw, false is ccw
+    global motor_steps
     for i in range(round(degrees / 360 * STEPS_PER_ROTATION)):
         for pin in range(len(MOTOR_PINS)):
             GPIO.output(MOTOR_PINS[pin], STEP_SEQUENCE[motor_steps % len(STEP_SEQUENCE)][pin])
@@ -77,6 +78,7 @@ while True:
                     image = frame.array
                     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert to HSV for use with inRange
                     mask = cv2.inRange(hsv_image, lower_color_bound, upper_color_bound) # create a bit mask
+
                     # of what pixels fall within the color range
                     if numpy.sum(mask) > 200000: # my cat detected! 500x400 square of her coat was found
                         # -- potentially disable light, might mess with their vision more -- #
