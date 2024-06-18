f1121fc5b5a3eaabdefc4a27841e2a1401e66a2c
Ben Thompson <ben.t11@hotmail.com>
2024-06-18 12:41:41 -0700

    close camera when done

--- core.py
+++ core.py
@@ -23,18 +23,18 @@ STEPS_PER_ROTATION = 4096
 STEP_TIME = 0.001 # 0.002 # time to wait between each step
 # if set too low, steps will be skipped
 STEP_SEQUENCE = [[1, 0, 0, 1],
-                 [1, 0, 0, 0],
-                 [1, 1, 0, 0],
-                 [0, 1, 0, 0],
-                 [0, 1, 1, 0],
-                 [0, 0, 1, 0],
-                 [0, 0, 1, 1],
-                 [0, 0, 0, 1]]
+				 [1, 0, 0, 0],
+				 [1, 1, 0, 0],
+				 [0, 1, 0, 0],
+				 [0, 1, 1, 0],
+				 [0, 0, 1, 0],
+				 [0, 0, 1, 1],
+				 [0, 0, 0, 1]]
 
 for i in range(len(MOTOR_PINS)):
-    print(i)
-    GPIO.setup(MOTOR_PINS[i], GPIO.OUT)
-    GPIO.output(MOTOR_PINS[i], GPIO.LOW)
+	print(i)
+	GPIO.setup(MOTOR_PINS[i], GPIO.OUT)
+	GPIO.output(MOTOR_PINS[i], GPIO.LOW)
 
 lower_color_bound = numpy.array([14, 0, 0])
 upper_color_bound = numpy.array([30, 154, 154]) # my cat's coat color
@@ -45,61 +45,63 @@ CAMERA_Y_RES = 1088
 motor_steps = 0
 
 def runMotor(degrees, direction): # direction: true is cw, false is ccw
-    global motor_steps
-    for i in range(round(degrees / 360 * STEPS_PER_ROTATION)):
-        for pin in range(len(MOTOR_PINS)):
-            GPIO.output(MOTOR_PINS[pin], STEP_SEQUENCE[motor_steps % len(STEP_SEQUENCE)][pin])
+	global motor_steps
+	for i in range(round(degrees / 360 * STEPS_PER_ROTATION)):
+		for pin in range(len(MOTOR_PINS)):
+			GPIO.output(MOTOR_PINS[pin], STEP_SEQUENCE[motor_steps % len(STEP_SEQUENCE)][pin])
 
-        if direction:
-            motor_steps -= 1
-        else:
-            motor_steps += 1
+		if direction:
+			motor_steps -= 1
+		else:
+			motor_steps += 1
 
-        time.sleep(STEP_TIME)
+		time.sleep(STEP_TIME)
 
 def openTray():
-    print("OPENING TRAY")
-    runMotor(TRAY_EXTEND_DEGREES, False)
+	print("OPENING TRAY")
+	runMotor(TRAY_EXTEND_DEGREES, False)
 
 def closeTray():
-    print("CLOSING TRAY")
-    runMotor(TRAY_EXTEND_DEGREES, True)
+	print("CLOSING TRAY")
+	runMotor(TRAY_EXTEND_DEGREES, True)
 
 while True:
-    if GPIO.input(SWITCH_PIN):
-        print("ACTIVATED")
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        
-        try:
-            with PiCamera() as cam:
-                cam.resolution = (CAMERA_X_RES, CAMERA_Y_RES) # set up camera
-                cam.framerate = 30
-                feed = PiRGBArray(cam, size = (CAMERA_X_RES, CAMERA_Y_RES))
-                time.sleep(1) # let sensor adjust exposure etc.
-                
-                for frame in cam.capture_continuous(feed, format = "bgr", use_video_port = True):
-                    image = frame.array
-                    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert to HSV for use with inRange
-                    mask = cv2.inRange(hsv_image, lower_color_bound, upper_color_bound) # create a bit mask
+	if GPIO.input(SWITCH_PIN):
+		print("ACTIVATED")
+		GPIO.output(LED_PIN, GPIO.HIGH)
+		
+		with PiCamera() as cam:
+			try:
+				cam.resolution = (CAMERA_X_RES, CAMERA_Y_RES) # set up camera
+				cam.framerate = 30
+				feed = PiRGBArray(cam, size = (CAMERA_X_RES, CAMERA_Y_RES))
+				time.sleep(1) # let sensor adjust exposure etc.
+				
+				for frame in cam.capture_continuous(feed, format = "bgr", use_video_port = True):
+					image = frame.array
+					hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert to HSV for use with inRange
+					mask = cv2.inRange(hsv_image, lower_color_bound, upper_color_bound) # create a bit mask
 
-                    # of what pixels fall within the color range
-                    if numpy.sum(mask) > 10:#384000: # my cat detected! 500x400 square of her coat was found
-                        # -- potentially disable light, might mess with their vision more -- #
-                        openTray() # open the food tray
+					# of what pixels fall within the color range
+					if numpy.sum(mask) > 10:#384000: # my cat detected! 500x400 square of her coat was found
+						# -- potentially disable light, might mess with their vision more -- #
+						openTray() # open the food tray
 
-                        while GPIO.input(SWITCH_PIN): # wait for the kitty to eat
-                            time.sleep(1)
+						while GPIO.input(SWITCH_PIN): # wait for the kitty to eat
+							time.sleep(1)
 
-                        GPIO.output(LED_PIN, GPIO.LOW)
-                        feed.truncate(0)
-                        closeTray() # close the food tray
-                        break
+						GPIO.output(LED_PIN, GPIO.LOW)
+						feed.truncate(0)
+						closeTray() # close the food tray
+						break
 
-                    # clear stream
-                    feed.truncate(0)
-        except:
-            print("CAMERA NOT CONNECTED PROPERLY/ENABLED")
+					# clear stream
+					feed.truncate(0)
+			finally:
+				cam.close()
+			except:
+				print("CAMERA NOT CONNECTED PROPERLY/ENABLED")
 
 
-    GPIO.output(LED_PIN, GPIO.LOW)
-    time.sleep(0.1)
+	GPIO.output(LED_PIN, GPIO.LOW)
+	time.sleep(0.1)
