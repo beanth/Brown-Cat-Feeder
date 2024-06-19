3b2083555be892307d7a42f6e0b2cba3f852f86c
beanth <rcburner@hotmail.com>
2024-06-18 22:24:16 -0700

    remove unneeded encode

--- capture.py
+++ capture.py
@@ -4,14 +4,13 @@ import numpy
 from picamera2 import Picamera2, Preview
 from datetime import datetime
 
-_, image_encoded = cv2.imencode('.jpg', cv2.imread('images/no-sample.jpg'))
-
 def capture_loop(data):
 	cam = Picamera2()
 	cam_config = cam.create_preview_configuration()
 	cam.configure(cam_config)
 	cam.start_preview(Preview.NULL)
 	cam.start()
+	
 	while data[2]:
 		time.sleep(1)
 		_, image_encoded = cv2.imencode('.jpg', cam.capture_array())
