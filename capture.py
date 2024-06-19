18158551ea55428e89d93227d596c713953000ad
beanth <rcburner@hotmail.com>
2024-06-18 21:46:36 -0700

    start using picamera2, get view fetching functionality working

--- capture.py
+++ capture.py
@@ -1,15 +1,21 @@
 import cv2
 import time
 import numpy
+from picamera2 import Picamera2, Preview
 from datetime import datetime
 
 _, image_encoded = cv2.imencode('.jpg', cv2.imread('images/no-sample.jpg'))
 
 def capture_loop(data):
-	# camera capture logic
-	# with PiCamera() as cam:
-	#data[1].append([datetime.now(), 13])
+	cam = Picamera2()
+	cam_config = cam.create_preview_configuration()
+	cam.configure(cam_config)
+	cam.start_preview(Preview.NULL)
+	cam.start()
 	while True:
+		time.sleep(1)
+		_, image_encoded = cv2.imencode('.jpg', cam.capture_array())
 		data[0] = image_encoded
 		data[1].append([datetime.now(), 30])
-		time.sleep(1)
+	
+	cam.stop()
