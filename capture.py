505eaf2c90d59601efb9311bbbfc7785ed5f67f2
beanth <rcburner@hotmail.com>
2024-06-20 18:27:19 -0700

    zero out noisy bits of image

--- capture.py
+++ capture.py
@@ -20,6 +20,16 @@ def capture_loop(data):
 		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 		mask = cv2.inRange(hsv, lower_color_bound, upper_color_bound)
+		height = image.shape[0]
+		width = image.shape[1]
+		########################
+		######### TODO ######### MAKE MASK SIZE AND POSITION CUSTOMIZABLE
+		########################
+		for y in range(90): # cut off some of the noise
+			for x in range(width):
+				mask[y][x] = 0
+			if y > 100:
+				break
 		num_food = numpy.sum(mask)
 		result = cv2.bitwise_and(image, image, mask=mask)
 		result = cv2.putText(result, str(num_food), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA, False)
