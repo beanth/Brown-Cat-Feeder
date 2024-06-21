bb223d2f83c83b87fc87f3158054ddc02c3e8fe4
beanth <rcburner@hotmail.com>
2024-06-20 17:22:11 -0700

    begin color recognition

--- capture.py
+++ capture.py
@@ -4,6 +4,9 @@ import numpy
 from picamera2 import Picamera2, Preview
 from datetime import datetime
 
+lower_color_bound = (5, 50, 50)
+upper_color_bound = (15, 255, 255)
+
 def capture_loop(data):
 	cam = Picamera2()
 	cam_config = cam.create_preview_configuration()
@@ -15,6 +18,7 @@ def capture_loop(data):
 		time.sleep(1)
 		image = cam.capture_array()
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		mask = cv2.inRange(image, lower_color_bound, upper_color_bound)
 		_, image_encoded = cv2.imencode('.jpg', image)
 		data[0] = image_encoded
 		data[1].append([datetime.now(), 30])
