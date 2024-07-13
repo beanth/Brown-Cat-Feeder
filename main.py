5bbef7f2185cb9e19fe8b26864093300bcbd314d
beanth <rcburner@hotmail.com>
2024-07-12 22:17:12 -0700

    convert image to video stream

--- main.py
+++ main.py
@@ -10,17 +10,25 @@ from datetime import datetime
 app = Flask(__name__)
 samples = [cv2.imencode('.jpg', cv2.imread('images/no-sample.jpg'))[1], [], True]
 
+def gen_frame():
+	while True:
+		frame = samples[0].tobytes()
+		yield (b'--frame\r\n'
+			b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/', methods = ['GET'])
 def main():
 	if (request.method == 'GET'):
-		data = samples[0].tobytes()
-		res = Response(data)
-		res.headers["Content-Type"] = "image/jpeg"
-		res.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-		res.headers["Pragma"] = "no-cache"
-		res.headers["Expires"] = "0"
-		res.cache_control.max_age = 0
-		return res
+		#data = samples[0].tobytes()
+		#res = Response(data, mimetype='multipart/x-mixed-replace; boundary=frame')
+		#res.headers["Content-Type"] = "image/jpeg"
+		#res.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+		#res.headers["Pragma"] = "no-cache"
+		#res.headers["Expires"] = "0"
+		#res.cache_control.max_age = 0
+		#return res
+		return Response(gen_frame(),
+			mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/data', methods = ['GET'])
 def fetch_data():
@@ -41,6 +49,6 @@ if __name__ == '__main__':
 	thread = threading.Thread(target=capture_loop, args=(samples,))
 	thread.start()
 
-	app.run(debug=False, host="0.0.0.0", port=5010)
+	app.run(debug=False, host="0.0.0.0", port=5010, threaded=True)
 
 	thread.join()
