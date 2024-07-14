0727f93a86210ebf2e5bf554a97a343d901a5d24
beanth <rcburner@hotmail.com>
2024-07-14 13:42:24 -0700

    fix excessive cpu and ram usage from sending same cam frame multiple times

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy
 import threading
 import signal
+import logging
 import sys
 from flask import Flask, jsonify, request, Response
 from capture import capture_loop
@@ -9,28 +10,34 @@ from datetime import datetime
   
 app = Flask(__name__)
 app.logger.setLevel(logging.ERROR)
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 samples = [cv2.imencode('.jpg', cv2.imread('images/no-sample.jpg'))[1], [], True]
 
 def gen_frame():
+	oldFrame = 0
 	while True:
 		frame = samples[0].tobytes()
-		yield (b'--frame\r\n'
-			b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+		if (frame != oldFrame):
+			oldFrame = frame
+			yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+		else:
+			yield (b'')
 
 @app.route('/', methods = ['GET'])
 def main():
 	if (request.method == 'GET'):
 		#data = samples[0].tobytes()
-		#res = Response(data, mimetype='multipart/x-mixed-replace; boundary=frame')
-		#res.headers["Content-Type"] = "image/jpeg"
-		#res.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-		#res.headers["Pragma"] = "no-cache"
-		#res.headers["Expires"] = "0"
-		#res.cache_control.max_age = 0
-		#return res
-		return Response(gen_frame(),
+		res = Response(gen_frame(),
 			mimetype='multipart/x-mixed-replace; boundary=frame')
+		#res.headers["Content-Type"] = "image/jpeg"
+		res.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+		res.headers["Pragma"] = "no-cache"
+		res.headers["Expires"] = "0"
+		res.cache_control.max_age = 0
+		return res
 
 @app.route('/data', methods = ['GET'])
 def fetch_data():
